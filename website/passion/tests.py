17c6814e12eb1ec8843e8aa975fd8b28dcf19ddc
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 19:46:38 +0545

    added a dummy tests.py in main app

---
+++ website/passion/tests.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+
+class TestHomePage(TestCase):
+
+    def test_uses_base_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "layout/base.html")
