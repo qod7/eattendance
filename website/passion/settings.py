cc85f49c50d95b2288334eb00ff240d0ec184f81
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 18:46:23 +0545

    added settings for static and templates

--- website/passion/settings.py
+++ website/passion/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'passion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticdeploy'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
