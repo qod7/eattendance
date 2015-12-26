696dfa2f239d335158c551a42cdad608c1c31ffc
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 18:43:07 +0545

    added apps: api, organization, reseller

--- website/passion/settings.py
+++ website/passion/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api',
+    'organization',
+    'reseller',
 ]
 
 MIDDLEWARE_CLASSES = [
