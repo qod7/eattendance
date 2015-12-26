2e308df8a9bb04ba4ce476c1529fc5a59ea0a73b
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 22:50:32 +0545

    added reseller

--- website/organization/models.py
+++ website/organization/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from reseller.models import Reseller
 
 
 class Organization(models.Model):
@@ -6,6 +7,7 @@ class Organization(models.Model):
     A generic organization. From here we can inherit school, bank etc.
     """
     name = models.CharField("Name", max_length=100)
+    reseller = models.ForeignKey(Reseller)
 
     class Meta:
         verbose_name = "Organization"
