200746a0370ee395d9d8e16480cf70c34580702c
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 19:57:13 +0545

    changed liveservertestcase to staticliveservertestcase

--- website/functional_tests/test_all_users.py
+++ website/functional_tests/test_all_users.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from django.core.urlresolvers import reverse
-from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class HomeNewVisitorTest(LiveServerTestCase):
+class HomeNewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
