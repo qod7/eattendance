f37214dafcffb566f644a87c8396ef1c452ecf5e
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 21:29:56 +0545

    added a generic organization

--- website/organization/models.py
+++ website/organization/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    """
+    A generic organization. From here we can inherit school, bank etc.
+    """
+    name = models.CharField("Name", max_length=100)
+
+    class Meta:
+        verbose_name = "Organization"
+        verbose_name_plural = "Organizations"
+
+    def __str__(self):
+        return self.name
