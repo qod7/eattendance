239093ff4b60e46f59492858b01faa16a1bc2f61
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 20:54:46 +0545

    now the database is postgresql

--- website/passion/settings/base.py
+++ website/passion/settings/base.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'passion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'passion',
+        'USER': 'passion',
+        'PASSWORD': 'attendance@passion',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
