ed5e5b3c8b410016b078cf076f0e74b567ac61e0
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 19:42:50 +0545

    this is where we write functional tests

---
+++ website/functional_tests/test_all_users.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.testing import LiveServerTestCase
+
+
+class HomeNewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_home_title(self):
+        self.browser.get(self.get_full_url("home"))
+        self.assertIn("Passion", self.browser.title)
