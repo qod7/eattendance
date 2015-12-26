3e670fbac052c5ae1078531396fccef9ee8bb8be
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-26 22:49:15 +0545

    wrote necessary tests

--- website/reseller/tests.py
+++ website/reseller/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Reseller
+
+from organization.models import Organization
+
+
+class ResellerTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user('john', 'john@john.com', 'ramramtau')
+        rslr = Reseller.objects.create(user=user)
+        Organization.objects.create(name="Org A", reseller=rslr)
+        Organization.objects.create(name="Org B", reseller=rslr)
+
+    # def tearDown(self):
+    #     pass
+
+    def test_reseller_default_values(self):
+        """
+        Test whether reseller is created correctly with default values
+        """
+        rslr = Reseller.objects.first()
+        self.assertEqual(rslr.contact, "")
+        self.assertEqual(rslr.remarks, "")
+
+    def test_reseller_disable(self):
+        rslr = Reseller.objects.first()
+        rslr.deactivate_account()
+        self.assertEqual(rslr.user.is_active, False)
+
+    def test_reseller_enable(self):
+        rslr = Reseller.objects.first()
+        rslr.activate_account()
+        self.assertEqual(rslr.user.is_active, True)
+
+    def test_reseller_org_count(self):
+        rslr = Reseller.objects.first()
+        self.assertEqual(rslr.organization_count(), 2)
+
+    def test_reseller_can_create_org(self):
+        """
+        Can the reseller from setup create any organizations?
+        """
+        rslr = Reseller.objects.first()
+        self.assertEqual(rslr.can_create_organization(), False)
