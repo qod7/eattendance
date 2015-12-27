63a211e5620348cd71bbbb4d4db694e207ed4c9a
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-27 10:51:01 +0545

    added includes for the app urls in main urls.py

--- website/passion/urls.py
+++ website/passion/urls.py
@@ -1,26 +1,19 @@
-"""passion URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Import the include() function: from django.conf.urls import url, include
-    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from . import views
 
+from reseller import urls as reseller_urls
+from organization import urls as organization_urls
+from api import urls as api_urls
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^reseller/', include(reseller_urls, namespace='reseller')),
+    url(r'^organization/', include(organization_urls, namespace='organization')),
+    url(r'^api/', include(api_urls, namespace='api')),
+
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
         views.home_files, name='home-files'),
