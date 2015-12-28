b9010c020936bc703193ec89b3ace0aa28bae242
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-28 16:58:16 +0545

    superadmin urls

--- website/passion/urls.py
+++ website/passion/urls.py
@@ -5,15 +5,17 @@ from . import views
 
 from reseller import urls as reseller_urls
 from organization import urls as organization_urls
+from superadmin import urls as superadmin_urls
 from api import urls as api_urls
-from useraccounts import urls as login_urls
+from useraccounts import urls as account_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^login/', include(login_urls, namespace='account')),
+    url(r'^account/', include(account_urls, namespace='account')),
     url(r'^reseller/', include(reseller_urls, namespace='reseller')),
     url(r'^organization/', include(organization_urls, namespace='organization')),
+    url(r'^superadmin/', include(superadmin_urls, namespace='superadmin')),
     url(r'^api/', include(api_urls, namespace='api')),
 
     url(r'^$', views.HomeView.as_view(), name='home'),
