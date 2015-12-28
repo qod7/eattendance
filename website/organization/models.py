268ee20c3af29d6cf24283ad40aef64ff270f6b4
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-28 15:56:02 +0545

    added user to organization

--- website/organization/models.py
+++ website/organization/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from reseller.models import Reseller
 
+from django.contrib.auth.models import User
+
 
 class Organization(models.Model):
     """
     A generic organization. From here we can inherit school, bank etc.
     """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField("Name", max_length=100)
     reseller = models.ForeignKey(Reseller, on_delete=models.CASCADE)
 
