4ec5e8edbfc0fb54ce48a49f63aaee4446c8f33e
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-28 09:50:55 +0545

    urls and views for useraccounts, namespace account

--- website/passion/urls.py
+++ website/passion/urls.py
@@ -6,15 +6,17 @@ from . import views
 from reseller import urls as reseller_urls
 from organization import urls as organization_urls
 from api import urls as api_urls
+from useraccounts import urls as login_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
+    url(r'^login/', include(login_urls, namespace='account')),
     url(r'^reseller/', include(reseller_urls, namespace='reseller')),
     url(r'^organization/', include(organization_urls, namespace='organization')),
     url(r'^api/', include(api_urls, namespace='api')),
 
-    url(r'^$', views.LoginView.as_view(), name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
         views.home_files, name='home-files'),
 ]
