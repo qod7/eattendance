7c00dd1d2919693fb6d1a6684a11abc198105511
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-28 16:07:58 +0545

    added login views and related tests; todo: test by simulating requests

--- website/useraccounts/tests.py
+++ website/useraccounts/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your tests here.
+from organization.models import Organization
+from reseller.models import Reseller
+
+
+class LoginTestCase(TestCase):
+
+    """Tests for Login"""
+    # todo: test own functions by simulating HttpRequests
+
+    def setUp(self):
+        org = User.objects.create_user('org', 'john@john.com', 'ramramtau')
+        reseller = User.objects.create_user('reseller', 'john@john.com', 'ramramtau')
+        superadmin = User.objects.create_user('superadmin', 'john@john.com', 'ramramtau')
+        deactivated_user = User.objects.create_user('deac', 'john@john.com', 'ramramtau')
+
+        rslr = Reseller.objects.create(user=reseller)
+        Organization.objects.create(name="Org A", reseller=rslr, user=org)
+
+        superadmin.is_staff = True
+        superadmin.save()
+
+        deactivated_user.is_active = False
+        deactivated_user.save()
+
+    def test_login(self):
+        user = auth.authenticate(username='org', password='ramramtau')
+        self.assertNotEqual(user, 'None')
+
+    def test_org_login(self):
+        user = auth.authenticate(username='org', password='ramramtau')
+        self.assertEqual(Organization.objects.filter(user=user).exists(), True)
+
+    def test_resller_login(self):
+        user = auth.authenticate(username='reseller', password='ramramtau')
+        self.assertEqual(Reseller.objects.filter(user=user).exists(), True)
+
+    def test_superadmin_login(self):
+        user = auth.authenticate(username='superadmin', password='ramramtau')
+        self.assertEqual(user.is_staff, True)
+
+    def test_deactivated_user_login(self):
+        user = auth.authenticate(username='deac', password='ramramtau')
+        self.assertEqual(user.username, 'deac')
--- website/useraccounts/views.py
+++ website/useraccounts/views.py
@@ -1,5 +1,68 @@
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+
+from reseller.models import Reseller
+from organization.models import Organization
 
 
-class LoginView(TemplateView):
-    template_name = "useraccounts/login.html"
+class LoginView(View):
+
+    '''
+    View for logging in a user.
+    '''
+
+    def get(self, request):
+        if request.user.is_authenticated():
+            # return self.check_user_type_and_redirect(request)
+            return HttpResponse("Logged In!")
+
+        return render(self.request, 'useraccounts/login.html', {'title': 'Login'})
+
+    def post(self, request):
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth.login(self.request, user)
+                # return self.check_user_type_and_redirect(self.request)
+                return HttpResponse("Logged In!")
+            else:
+                messages.error(
+                    self.request,
+                    "Sorry, your account has been deactivated.\
+                     Please contact the administrator.")
+                return redirect('accounts:login')
+        else:
+            messages.error(self.request, "Invalid username or password.")
+            return redirect('accounts:login')
+
+    def check_user_type_and_redirect(self, request):
+        '''
+        check whether the user is a superadmin, reseller, or organization
+        and redirect them to their respective dashboard
+        '''
+        if request.user.is_staff:
+            return redirect('superadmin:home')
+        elif Reseller.objects.filter(user=request.user).exists():
+            return redirect('reseller:home')
+        elif Organization.objects.filter(user=request.user).exists():
+            return redirect('organization:home')
+        else:
+            auth.logout(request)
+            messages.error(self.request,
+                           "Your account has been deleted.\
+                            Please contact the administrator.")
+            return redirect('accounts:login')
+
+
+@login_required(login_url='accounts:login')
+def logout(request):
+    auth.logout(request)
+    return redirect('accounts:login')
