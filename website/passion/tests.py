d455196c90dd2cc556cb022bdacb2fe4c4b28373
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 08:55:31 +0545

    removed test for base template

--- website/passion/tests.py
+++ website/passion/tests.py
@@ -5,5 +5,6 @@ from django.core.urlresolvers import reverse
 class TestHomePage(TestCase):
 
     def test_uses_base_template(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "layout/base.html")
+        # response = self.client.get(reverse("home"))
+        # self.assertTemplateUsed(response, "layout/base.html")
+        pass
