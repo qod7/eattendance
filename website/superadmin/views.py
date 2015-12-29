e36ee75d7355767f582639d305f34f51b8407542
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 14:14:36 +0545

    added title to context, renamed CreateResellerView, docstrings

--- website/superadmin/views.py
+++ website/superadmin/views.py
@@ -13,16 +13,55 @@ class SuperAdminTestMixin(LoginRequiredMixin, UserPassesTestMixin):
 
 
 class HomeView(SuperAdminTestMixin, TemplateView):
+
+    """
+    Dashboard that shows overall statistics.
+    """
     template_name = "superadmin/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['title'] = "Dashboard"
+        return context
 
 
 class ListResellersView(SuperAdminTestMixin, TemplateView):
+
+    """
+    Lists the current resellers with various fields.
+    """
     template_name = "superadmin/list_resellers.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(ListResellersView, self).get_context_data(**kwargs)
+        context['title'] = "List Resellers"
+        return context
 
-class CreateResellerView(SuperAdminTestMixin, TemplateView):
-    template_name = "superadmin/create_reseller.html"
+
+class AddResellerView(SuperAdminTestMixin, TemplateView):
+
+    """
+    Shows a form to add a reseller.
+    """
+    template_name = "superadmin/add_reseller.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AddResellerView, self).get_context_data(**kwargs)
+        context['title'] = "Add a Reseller"
+        return context
 
 
 class SettingsView(SuperAdminTestMixin, TemplateView):
+
+    """
+    Edit user settings.
+    - Change Password
+    - Change email address
+    - Change first_name and last_name
+    """
     template_name = "superadmin/settings.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SettingsView, self).get_context_data(**kwargs)
+        context['title'] = "Settings"
+        return context
