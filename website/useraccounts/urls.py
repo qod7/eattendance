ab2cd2e61214d252ec44c34ff5823d90cab37b0c
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 08:30:08 +0545

    added a redirectview on accounts/ to accounts/login

--- website/useraccounts/urls.py
+++ website/useraccounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.LoginView.as_view(), name='login'),
+    url(r'^$', RedirectView.as_view(pattern_name='account:login')),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.logout, name='logout'),
 ]
