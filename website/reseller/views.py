b242cdcc78d604e2b9d9f996cf274fe519a08daa
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 09:50:37 +0545

    added resellertestmixin; #todo: check reseller using groups

--- website/reseller/views.py
+++ website/reseller/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class HomeView(TemplateView):
+class ResellerTestMixin(LoginRequiredMixin, UserPassesTestMixin):
+
+    """
+    Checks if user is logged in and if user is a reseller.
+    """
+
+    def test_func(self):
+        # return self.request.user.reseller.exists()
+        pass
+
+
+class HomeView(ResellerTestMixin, TemplateView):
     template_name = "reseller/base.html"
