85f8eac680e083bc0824df110227709b313c6ee5
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 21:20:57 +0545

    urls for forgot password

--- website/useraccounts/urls.py
+++ website/useraccounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='account:login')),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^reset-password/$', views.ResetPasswordView.as_view(), name='reset_password'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.password_reset_confirm, name='password_reset_confirm'),
 ]
