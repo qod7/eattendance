f760ad4d14ca5da0859f2722138bde6ace1284bb
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 14:06:08 +0545

    added a form for login; haven't implemented yet

---
+++ website/useraccounts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+
+    """
+    Form for logging in a user.
+    """
+
+    username = forms.CharField()
+    password = forms.CharField()
