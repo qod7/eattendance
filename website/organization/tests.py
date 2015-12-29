8d36677390023e69f5557af2231ef75ac737cc42
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 17:47:20 +0545

    added users to orga

--- website/organization/tests.py
+++ website/organization/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # , Group
 
 from .models import Organization
 from reseller.models import Reseller
@@ -12,13 +12,23 @@ class OrganizationTestCase(TestCase):
     """
 
     def setUp(self):
-        user = User.objects.create_user('john', 'john@john.com', 'ramramtau')
-        rslr = Reseller.objects.create(user=user)
-        Organization.objects.create(name="Org A", reseller=rslr)
-        Organization.objects.create(name="Org B", reseller=rslr)
+        orga = User.objects.create_user('orga', 'john@john.com', 'ramramtau')
+        orgb = User.objects.create_user('orgb', 'john@john.com', 'ramramtau')
+        resellerUser = User.objects.create_user('john', 'john@john.com', 'ramramtau')
+
+        rslr = Reseller.objects.create(user=resellerUser)
+        Organization.objects.create(name="Org A", reseller=rslr, user=orga)
+        Organization.objects.create(name="Org B", reseller=rslr, user=orgb)
 
     def test_only_org_can_access_dashboard(self):
         # try to login with a reseller # verify rejected
         # try to login with a superadmin # verify rejected
         # try to login with an organization # verify accepted
         pass
+
+    # def test_group_organization_exists(self):
+    #     """
+    #     Group Organization needs to exists for the checks on the
+    #     organization.views.OrganizationTestMixin level to work.
+    #     """
+    #     self.assertEqual(Group.objects.filter(name__icontains='Organization').exists(), True)
