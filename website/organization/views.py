899a1c56e5c9b41a2fb8f2b79748d628e8ad37d4
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 14:09:55 +0545

    added orgtestmixin

--- website/organization/views.py
+++ website/organization/views.py
@@ -1,5 +1,19 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Organization
 
 
-class HomeView(TemplateView):
+class OrganizationTestMixin(LoginRequiredMixin, UserPassesTestMixin):
+
+    """
+    Checks if user is logged in and if user is a organization.
+    """
+
+    def test_func(self):
+        # return self.request.user.organization.exists()
+        # todo: find a way to do this using groups
+        return Organization.objects.filter(user=self.request.user).exists()
+
+
+class HomeView(OrganizationTestMixin, TemplateView):
     template_name = "organization/base.html"
