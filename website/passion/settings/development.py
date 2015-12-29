a454864a92cae476ab553a25e67a3a93e74d0109
Ashish Acharya <ashish.acharya14@gmail.com>
2015-12-29 08:50:53 +0545

    added email backends

--- website/passion/settings/development.py
+++ website/passion/settings/development.py
@@ -3,3 +3,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'emailcopies')
