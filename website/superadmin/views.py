1dd91f9687895933c646647d4d225c78cb6be1f4
Ashish Acharya <ashish.acharya14@gmail.com>
2016-01-09 11:39:16 +0545

    working on formview for adding reseller

--- website/superadmin/views.py
+++ website/superadmin/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse
+
+from .forms import AddResellerForm
 
 
 class SuperAdminTestMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -38,12 +42,22 @@ class ListResellersView(SuperAdminTestMixin, TemplateView):
         return context
 
 
-class AddResellerView(SuperAdminTestMixin, TemplateView):
+class AddResellerView(SuperAdminTestMixin, FormView):
 
     """
-    Shows a form to add a reseller.
+    Shows and processes form to add a reseller.
     """
-    template_name = "superadmin/add_reseller.html"
+    template_name = 'superadmin/add_reseller.html'
+    form_class = AddResellerForm
+
+    def get_success_url(self):
+        return reverse('superadmin:list_resellers')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        # form.send_email()
+        return super(AddResellerView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(AddResellerView, self).get_context_data(**kwargs)
