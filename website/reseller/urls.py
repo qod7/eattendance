158ef298a6aaf4c8be2e5c334fafe2c148c407ca
Ashish Acharya <ashish.acharya14@gmail.com>
2016-01-21 18:32:57 +0545

    addorganizationsview -> addorganizationview

--- website/reseller/urls.py
+++ website/reseller/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^list-organizations/$', views.ListOrganizationsView.as_view(), name='list_organizations'),
-    url(r'^add-organization/$', views.AddOrganizationsView.as_view(), name='add_organization'),
+    url(r'^add-organization/$', views.AddOrganizationView.as_view(), name='add_organization'),
     url(r'^settings/$', views.SettingsView.as_view(), name='settings'),
 ]
