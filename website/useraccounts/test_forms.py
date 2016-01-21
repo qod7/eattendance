aaefaf2720538583e749fd37bd5f6ace401d8ef4
Ashish Acharya <ashish.acharya14@gmail.com>
2016-01-21 21:08:05 +0545

    tests for generic user creation form

--- website/useraccounts/test_forms.py
+++ website/useraccounts/test_forms.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from django import forms
 
 from .forms import GenericUserCreationForm
 
@@ -6,29 +8,89 @@ from .forms import GenericUserCreationForm
 class GenericUserCreationFormTestCase(TestCase):
 
     def setUp(self):
-        pass
+        User.objects.create_user('existing', 'existing@existing.com', 'ramramtau')
 
-    def test_template_rendered(self):
-        pass
+    def test_first_name_too_long(self):
+        first_name = "thisisaverylongfirstnamehopefullyitsnotallowed"
+        last_name = "valid"
+        email = "valid@valid.com"
 
-    def test_create_user(self):
-        pass
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
 
-    def test_too_long_first_name(self):
-        pass
+        form = GenericUserCreationForm(form_data)
 
-    def test_too_long_last_name(self):
-        pass
+        self.assertFalse(form.is_valid())
+        # self.assertEquals(form.errors['first_name'], ["First name is too long."])
+
+    def test_last_name_too_long(self):
+        first_name = "valid"
+        last_name = "thisisaverylonglastnamehopefullyitsnotallowed"
+        email = "valid@valid.com"
+
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
+
+        form = GenericUserCreationForm(form_data)
+
+        self.assertFalse(form.is_valid())
+        # self.assertEquals(form.errors['last_name'], ["Last name is too long."])
 
     def test_existing_email(self):
-        pass
+        first_name = "valid"
+        last_name = "valid"
+        email = 'existing@existing.com'
+
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
+
+        form = GenericUserCreationForm(form_data)
+
+        self.assertFalse(form.is_valid())
+        # self.assertFormError(form.errors['email'], "Email address")
 
     def test_invalid_email(self):
-        pass
+        first_name = "valid"
+        last_name = "valid"
+        email = 'invalid@invalid'
+
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
+
+        form = GenericUserCreationForm(form_data)
+
+        self.assertFalse(form.is_valid())
 
     def test_valid_form(self):
-        # redirects correctly
-        # shows message
+        first_name = "valid"
+        last_name = "valid"
+        email = "valid@valid.com"
+
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
+
+        form = GenericUserCreationForm(form_data)
+        self.assertTrue(form.is_valid())
+
+        user = form.save()
+        self.assertNotEqual(user, None)
+
+    def test_create_user(self):
         pass
 
     def test_email_to_user(self):
