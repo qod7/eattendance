d72cbcbcb6525eb4c60ee2111b52b819d5f72608
Ashish Acharya <ashish.acharya14@gmail.com>
2016-03-04 16:28:20 +0545

    added code for staff list view

--- website/organization/views.py
+++ website/organization/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.db.models.fields.related_descriptors import RelatedObjectDoesNotExist
 
-from .models import Organization
+from .models import Organization, Staff
 
 
 class OrganizationTestMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -23,7 +23,25 @@ class HomeView(OrganizationTestMixin, TemplateView):
 
 
 class StaffListView(OrganizationTestMixin, TemplateView):
+
+    """
+    Lists the current staff
+    """
+
+    context_object_name = 'staff_list'
     template_name = "organization/list_staff.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.organization = self.request.user.organization
+        return super(StaffListView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(StaffListView, self).get_context_data(**kwargs)
+        context['title'] = "List Staff"
+        return context
+
+    def get_queryset(self):
+        return Staff.objects.select_related('user').filter(organization=self.organization)
 
 
 class MessagesView(OrganizationTestMixin, TemplateView):
