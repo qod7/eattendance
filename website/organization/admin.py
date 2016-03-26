d8ef58eb084cbae7b5e96423ea2fd068fc5d3a29
Diwas Sharma <diwasblack@gmail.com>
2016-03-26 15:29:20 +0545

    Updated admin.py for organization

--- website/organization/admin.py
+++ website/organization/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Organization
+
+from .models import Organization, Message, Shift, Staff, Attendance
+from .models import Calendar, Day, Event
 
 admin.site.register(Organization)
+admin.site.register(Message)
+admin.site.register(Shift)
+admin.site.register(Staff)
+admin.site.register(Attendance)
+admin.site.register(Calendar)
+admin.site.register(Day)
+admin.site.register(Event)
