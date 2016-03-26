a30021a453ed42cdf683dad2f591bc49c344d904
Diwas Sharma <diwasblack@gmail.com>
2016-03-26 12:44:24 +0545

    Removed global restframework settings

--- website/passion/settings/base.py
+++ website/passion/settings/base.py
@@ -146,12 +146,3 @@ STATIC_ROOT = 'staticdeploy'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
