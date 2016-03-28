45b58969bebf231cb697c04ce8d7e43ed681be43
Diwas Sharma <diwasblack@gmail.com>
2016-03-28 21:53:27 +0545

    Token authentication for API

--- website/api/views.py
+++ website/api/views.py
@@ -2,6 +2,7 @@ import logging
 
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 from .serializers import AttendanceSerializer
 
@@ -13,7 +14,8 @@ class SaveAttendance(generics.CreateAPIView):
     """Save received attendance"""
 
     serializer_class = AttendanceSerializer
-    # permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = (TokenAuthentication, )
 
     def post(self, request, *args, **kwargs):
         logger.debug('Received request')
--- website/passion/urls.py
+++ website/passion/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.authtoken import views as drf_views
+
 from . import views
 
 from reseller import urls as reseller_urls
@@ -18,6 +20,7 @@ urlpatterns = [
 
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', drf_views.obtain_auth_token),
 
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
