b1b032f107374bee478926509e7578fc8d6039d9
Diwas Sharma <diwasblack@gmail.com>
2016-04-03 19:12:14 +0545

    Layout for staff api

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from organization.models import Attendance
+from organization.models import Attendance, Staff
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
+
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
--- website/api/views.py
+++ website/api/views.py
@@ -1,10 +1,14 @@
 import logging
 
+from django.contrib.auth.models import User
+
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
-from .serializers import AttendanceSerializer
+from organization.models import Staff
+from .serializers import AttendanceSerializer, StaffSerializer
 
 
 logger = logging.getLogger('django_debug')
@@ -15,7 +19,18 @@ class SaveAttendance(generics.CreateAPIView):
 
     serializer_class = AttendanceSerializer
     permission_classes = [IsAuthenticated, ]
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = [TokenAuthentication, ]
 
     def post(self, request, *args, **kwargs):
-        logger.debug('Received request')
+        logger.debug('Received request for attendance')
+
+
+class StaffViewSet(viewsets.ModelViewSet):
+    """DRF viewset for CRUD operation of staffs."""
+
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+    authentication_classes = [TokenAuthentication, ]
+
+    def create(self, request, *args, **kwargs):
+        logger.debug('Received request for staff create')
