e50c4b209601a91146b21539663adc35fa4a5e26
Ashish Acharya <ashish.acharya14@gmail.com>
2016-04-04 13:03:31 +0545

    now AddResellerFOrm uses GenericUserCreationForm

--- website/superadmin/forms.py
+++ website/superadmin/forms.py
@@ -1,141 +1,54 @@
 from django import forms
-from django.contrib.auth.models import User
-
-# for email
-from django.core.mail import EmailMultiAlternatives  # , get_connection
-from django.template.loader import get_template
-from django.template import Context
-# end
 
 from reseller.models import Reseller
+from useraccounts.forms import GenericUserCreationForm
 
 
-class AddResellerForm(forms.ModelForm):
+class AddResellerForm(GenericUserCreationForm):
     """
     Form for adding a new reseller.
     """
-    first_name = forms.CharField(
-        label='First Name',
-        max_length=30,
-        widget=forms.TextInput(attrs={'class': "form-control"})
+    contact = forms.CharField(
+        label="Reseller's Contact Number",
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': "form-control"}),
+        required=False
     )
-    last_name = forms.CharField(
-        label='Last Name',
-        max_length=30,
-        widget=forms.TextInput(attrs={'class': "form-control"})
+
+    remarks = forms.CharField(
+        max_length=100,
+        widget=forms.Textarea(attrs={'class': "form-control"}),
+        required=False
     )
-    email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': "form-control"})
+
+    organization_creation_limit = forms.IntegerField(
+        help_text="The maximum number of organizations\
+             that can be created.",
     )
 
     class Meta:
-        model = Reseller
-        fields = ['first_name', 'last_name', 'email', 'contact', 'remarks', 'organization_creation_limit']
         labels = {
             'remarks': "Additional Info",
             'organization_creation_limit': "Organization Limit",
         }
         help_texts = {
             'remarks': 'Optional. For internal use.',
-            'organization_creation_limit': "The maximum number of organizations\
-             that can be created.",
-        }
-        error_messages = {
-            'first_name': {
-                'max_length': "First name is too long.",
-            },
-
-            'last_name': {
-                'max_length': "Last name is too long.",
-            },
-        }
-        widgets = {
-            'contact': forms.TextInput(
-                attrs={'class': "form-control"}
-            ),
-            'remarks': forms.Textarea(
-                attrs={'class': "form-control"}
-            ),
-            # 'organization_creation_limit': forms.TextInput(
-            #     attrs={'class': "form-control"}
-            # )
         }
 
-    def clean_username(self, username):
-        # check if user is unique and if not, make it unique
-        while User.objects.filter(username=username).exists():
-            username = username + '1'
-        return username
-
-    def save(self, commit=False):
+    def save(self):
         """
         Creates the user and saves the reseller from form parameters.
         Sends login credentials in email.
         """
-        # get the reseller data from the form
-        reseller = super(AddResellerForm, self).save(commit=False)
+        # create user/send login credentials via email.
+        user = super(AddResellerForm, self).save()
+        assert user is not None
 
-        # get form parameters
-        first_name = self.cleaned_data.get('first_name').lower()
-        last_name = self.cleaned_data.get('last_name').lower()
-        email = self.cleaned_data.get('email').lower()
-
-        # generate a password
-        password = User.objects.make_random_password()
-
-        # create a user with username = fname_lname
-        username = self.clean_username(first_name + '_' + last_name)
-
-        # save first name, last name, email to the user
-        user = User.objects.create_user(username, email, password)
-        user.first_name = first_name.title()
-        user.last_name = last_name.title()
-        user.save()
-
-        # send an email to the user with his credentials
-        self.send_email(user, password)
-
-        # set this user to the model instance's user object
-        reseller.user = user
-        reseller.save()
-
-        super(AddResellerForm, self).save(commit=True)
-
-    def send_email(self, user, password):
-        """
-        Sends an email to the reseller with their login credentials
-        """
-        from_email = 'Passion Technologies<passion@gmail.com>'
-        template_subject = 'useraccounts/email_templates/subject.txt'
-        template_html = 'useraccounts/email_templates/email.html'
-        template_text = 'useraccounts/email_templates/email.txt'
-        subject = get_template(template_subject)
-        html = get_template(template_html)
-        text = get_template(template_text)
-
-        c = Context({
-            'user': user,
-            'password': password,
-            'name': self.cleaned_data['first_name'] + ' ' + self.cleaned_data['last_name'],
-        })
-
-        subject_content = subject.render(c)
-        text_content = text.render(c)
-        html_content = html.render(c)
-
-        msg = EmailMultiAlternatives(
-            subject=subject_content, body=text_content, from_email=from_email, to=[self.cleaned_data['email']])
-
-        msg.attach_alternative(html_content, "text/html")
-        msg.send(fail_silently=True)
-
-    def clean_email(self):
-        """
-        Makes sure that user with the same email address does not already exist.
-        """
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Email address "%(value)s" is already in use.',
-                                        params={'value': email},
-                                        code='already_exists')
-        return email
+        # create an organization
+        # todo: review pattern for inject user into form in Two Scoops.
+        Reseller.objects.create(
+            user=user,
+            contact=self.cleaned_data.get('contact'),
+            remarks=self.cleaned_data.get('remarks'),
+            organization_creation_limit=self.cleaned_data.get('organization_creation_limit'),
+        )
