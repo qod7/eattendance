4dad0f910b1803bcea73a39f7d32af28372d52b5
Diwas Sharma <diwasblack@gmail.com>
2016-04-06 20:00:51 +0545

    Removed nested serializer

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
 from organization.models import Attendance, Staff
@@ -10,14 +8,6 @@ class AttendanceSerializer(serializers.ModelSerializer):
         model = Attendance
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
-
-
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff
-
-    user = UserSerializer()
