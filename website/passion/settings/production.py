9935363467a69be54fcb3f69cc28acbf9c0655e1
Ashish Acharya <ashish.acharya14@gmail.com>
2016-04-08 12:16:10 +0545

    added gmail to production.py

--- website/passion/settings/production.py
+++ website/passion/settings/production.py
@@ -4,3 +4,10 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['eattendance.tk', '127.0.0.1']
+
+# gmail
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'attendance.passion@gmail.com'
+EMAIL_HOST_PASSWORD = 'mundrekovaisi'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
